6924df4bda155d9b1f6800ddbd8c4379d96fcb37
maisa08 <159453189+maisa08@users.noreply.github.com>
2024-11-12 10:40:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Kim SeokJin',
+    'Data de nascimento dele?',
+    '04/12/1992'
+)
+
+criaCartao(
+    'Kim SeokJin',
+    'Apelido mais famoso?',
+    'Worldwide handsome'
+)
+
+criaCartao(
+    'Kim SeokJin',
+    'Qual é o grupo de K-pop que ele participa?',
+    'Bangtan Boys/BTS'
+)
+
+criaCartao(
+    'Kim SeokJin',
+    'Cor favorita?',
+    'Azul'
+)
+
+criaCartao(
+    'Kim SeokJin',
+    'Comida favorita?',
+    'Naengmyeon'
+)
+
+criaCartao(
+    'Kim SeokJin',
+    'O que gosta de fazer?',
+    'Cozinhar, pescar e comer'
+)
